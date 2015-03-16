ec909c5a28fd1aecf578ce29c240cab7fc3d5c4d
Alberto Lagos <alberto.lagos@gmail.com>
2015-03-16 19:42:59 -0300

    See detail commits
    - Added regex to find mentions.
    - Keyboard always visible below the UIView that contains UITextView, UIButton and UILabel.

--- atMentions-Alberto/ViewController.m
+++ atMentions-Alberto/ViewController.m
@@ -11,11 +11,11 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *textView;
 @property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *keyboardHeight;
 
 
 
 - (void)keyboardWillShow:(id)sender;
-- (void)keyboardWillDisappear:(id)sender;
 @end
 
 @implementation ViewController
@@ -33,18 +33,57 @@
 }
 
 
+#pragma mark - UITextViewDelegate 
+
+- (void)textViewDidChange:(UITextView *)textView
+{
+    NSString *text = textView.text;
+    
+    NSArray *mentions = [self findMentions:text];
+    
+    NSLog(@"%s: %@", __func__, [self findMentions:text]);
+    
+}
+
+
+
+#pragma mark - Find mentions
+
+- (NSMutableArray *)findMentions:(NSString *)text
+{
+    
+    NSError *error = nil;
+    
+    NSRegularExpression *regex = [[NSRegularExpression alloc] initWithPattern:@"(@[a-zA-Z0-9_]+)"
+                                                                      options:NSRegularExpressionCaseInsensitive
+                                                                        error:&error];
+    
+    NSMutableArray *mentions = [NSMutableArray array];
+    
+    NSArray *matches = [regex matchesInString:text options:0 range:NSMakeRange(0, [text length])];
+    
+    for (NSTextCheckingResult *result in matches) {
+        
+        [mentions addObject:[text substringWithRange:result.range]];
+        
+    }
+    
+    return mentions;
+    
+}
+
 #pragma mark - NSNotification Keyboard methods.
 
-- (void)keyboardWillShow:(id)sender
+- (void)keyboardWillShow:(NSNotification *)notification
 {
+    NSDictionary *info = [notification userInfo];
+    NSValue *kbFrame = [info objectForKey:UIKeyboardFrameEndUserInfoKey];
+    CGRect keyboardFrame = [kbFrame CGRectValue];
     
+    CGFloat height = keyboardFrame.size.height;
+    self.keyboardHeight.constant = height + 8.0f;
 }
 
-- (void)keyboardWillDisappear:(id)sender
-{
-    
-}
-
-- (IBAction)uselessButtonDidPress:(id)sender {
+- (void)uselessButtonDidPress:(id)sender {
 }
 @end
