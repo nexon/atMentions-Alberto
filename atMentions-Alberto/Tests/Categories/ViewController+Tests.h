74981c9caa1cf5b19015264ae269deed68e75ee3
Alberto Lagos <alberto.lagos@gmail.com>
2015-03-17 21:12:21 -0300

    Added Tests.

--- atMentions-Alberto/Tests/Categories/ViewController+Tests.h
+++ atMentions-Alberto/Tests/Categories/ViewController+Tests.h
@@ -12,7 +12,15 @@
 @property (weak, nonatomic) IBOutlet UITextView *textView;
 @property (weak, nonatomic) IBOutlet UIView *containerView;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *keyboardHeight;
+@property (weak, nonatomic) IBOutlet UILabel *charLimitLabel;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *tableViewSpace;
+@property (strong, nonatomic) NSMutableArray  *contacts;
+@property (strong, nonatomic) NSMutableArray  *filteredArray;
+@property (strong, nonatomic) ATMContact      *selectedContact;
+@property (strong, nonatomic) NSString        *mention;
 
 - (void)keyboardWillShow:(id)sender;
 - (NSMutableArray *)findMentions:(NSString *)text;
+- (void)loadContacts;
 @end
--- atMentions-Alberto/Tests/ViewControllerTests.m
+++ atMentions-Alberto/Tests/ViewControllerTests.m
@@ -35,6 +35,31 @@
     XCTAssertTrue([mentions[0] isEqualToString:@"@insertingName"]);
 }
 
+- (void)testCharLimit
+{
+    id<UITextViewDelegate> textView = self.controller.textView.delegate;
+    NSString *textWithMentions = @"Lectus porta et parturient integer tortor, adipiscing quis dis facilisis vel nunc lectus nunc mauris porta pid, rhoncus auctor pulvinar! Risus adipiscing sit porttitor, integer turpis parturient diam etiam amet enim a nascetur sociis nisi etiam. Aenean risus pulvinar nunc sed nunc. Ac integer penatibus aliquet integer magna eros ultricies urna, elit scelerisque risus, phasellus? Penatibus porttitor adipiscing nunc nascetur. Massa urna amet, hac. Tristique, porta, sagittis porttitor enim. Integer";
+    
+    self.controller.textView.text = textWithMentions;
+    
+    BOOL response = [textView textView:self.controller.textView shouldChangeTextInRange:NSMakeRange(0, 0) replacementText:nil];
+    
+    XCTAssertTrue(!response);
+    
+}
+
+- (void)testFilterTable
+{
+    id<UITextViewDelegate> textView = self.controller.textView.delegate;
+    
+    NSString *textWithMentions = @"this is a test for @Shannon";
+    self.controller.textView.text = textWithMentions;
+    
+    [textView textViewDidChange:self.controller.textView];
+    
+    XCTAssertTrue(!self.controller.tableView.hidden);
+}
+
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
